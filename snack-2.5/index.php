9635bdabcadabb8c4a5bad645f374f008ca530eb
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-16 12:38:32 +0100

    snake 2.5 ended

---
+++ snack-2.5/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="GET">
+        <label for="first_number">First number:</label>
+        <input type="text" name="first_number">
+        <label for="second_number">Second number:</label>
+        <input type="text" name="second_number">
+        <button type="submit">calc.</button>
+    </form>
+    <p>
+        <?php 
+            if(isset($_GET["first_number"]) && isset($_GET["second_number"])){
+                echo "La media dei numeri è ". settype($_GET["first_number"], "integer") + settype($_GET["second_number"], "integer") / 2;
+            }
+        ?>
+    </p>
+</body>
+</html>
