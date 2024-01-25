005ee86974a21e48508e0bcbae3e479437095dfa
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-25 12:40:19 +0100

    methods remaning

---
+++ oop/pokemon.php
@@ -0,0 +1,18 @@
+<?php 
+    class Pokemon {
+        public $name;
+        public $race;
+        public $types;
+        public $locations;
+        public $moves;
+
+        function __construct(string $name, string $race, array $types, array $locations, array $moves)
+        {
+            $this->name;
+            $this->race;
+            $this->types;
+            $this->locations;
+            $this->moves;
+        }
+    }
+?>
